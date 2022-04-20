ad130fd90f5f4e58560916d5a09d723792fe7cc2
Sudhanshu Kumar <sudhiskpll@gmail.com>
2022-04-20 22:25:01 +0530

    completed weather pwa

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import { fetchWeather, fetchWeatherByCoordinates } from "./api/fetchWeather";
+import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const [current, setCurrent] = useState(true);
+
+  useEffect(() => {
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(currentWeather, showError);
+      } else {
+        setCurrent(false);
+      }
+    }
+
+    const currentWeather = async (position) => {
+      const { latitude, longitude } = position.coords;
+      const data = await fetchWeatherByCoordinates({
+        lat: latitude,
+        lon: longitude,
+      });
+      setWeather(data);
+    };
+
+    function showError(error) {
+      switch (error.code) {
+        case error.PERMISSION_DENIED:
+          console.log("User denied the request for Geolocation.");
+          break;
+        case error.POSITION_UNAVAILABLE:
+          console.log("Location information is unavailable.");
+          break;
+        case error.TIMEOUT:
+          console.log("The request to get user location timed out.");
+          break;
+        default:
+          console.log("An unknown error occurred.");
+          break;
+      }
+    }
+
+    getLocation();
+  }, []);
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+      setWeather(data);
+      setQuery("");
+      setCurrent(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={`main-container c${
+        weather.weather ? weather.weather[0].icon : ""
+      }`}
+    >
+      {current && weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+              className="city-icon"
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
+
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {!current && weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+              className="city-icon"
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/api/fetchWeather.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const URL = "https://api.openweathermap.org/data/2.5/weather";
+const API_KEY = "f33a484cf794d08d0148764789aaba32";
+
+export const fetchWeather = async (query) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      q: query,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
+  return data;
+};
+
+export const fetchWeatherByCoordinates = async ({ lat, lon }) => {
+  const { data } = await axios.get(URL, {
+    params: {
+      lat: lat,
+      lon: lon,
+      units: "metric",
+      APPID: API_KEY,
+    },
+  });
+  return data;
+};
